8dacb95488a97a4d0caa8a91cc8273c912d60186
apurbochaki-lab <apurbokumarchaki@gmail.com>
2026-02-23 20:56:50 +0600

    Count calculation logic function added

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hi from JS...")
+let interviewList = [];
+let rejectedList = [];
+let currentStatus = '';
+
+const totalCount = document.getElementById('totalCount');
+const jobsCount = document.getElementById('jobsCount');
+const interviewCount = document.getElementById('interviewCount');
+const rejectedCount = document.getElementById('rejectedCount');
+
+const parentNode = document.getElementById('all-cards');
+const filterSection = document.getElementById('filter-section');
+
+// All Count Calculations :
+function countCalculation() {
+    totalCount.innerText = parentNode.children.length;
+    jobsCount.innerText = parentNode.children.length;
+
+    interviewCount.innerText = interviewList.length;
+    rejectedCount.innerText = rejectedList.length;
+}
+countCalculation()
